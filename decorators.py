7c85e146e24b07be968154749be3809594df56ee
santdaniel <santdaniel333@gmail.com>
2021-05-06 06:51:57 -0300

    se agrego la hora

--- decorators.py
+++ decorators.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     tiempo=time.asctime()
-    return render_template('index.html')
+    return render_template('index.html',realtime=tiempo)
 if __name__=="__main__":
   app.run(debug = True, port=5000)
