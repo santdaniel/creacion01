9e9c8fd8c929c7ad5f14c1d0d4e358d077536e4d
santdaniel <santdaniel333@gmail.com>
2021-05-06 06:21:46 -0300

    version1

---
+++ decorators.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+import time
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    tiempo=time.asctime()
+    return render_template('index.html')
+if __name__=="__main__":
+  app.run(debug = True, port=5000)
